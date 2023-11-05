f03f5de9ff9fe104326e06783d2e64b9eaf230b6
BTwiiin <01176206@pw.edu.pl>
2023-11-05 13:39:39 +0100

    Added dependency injection

--- TelegramClone/App.xaml.cs
+++ TelegramClone/App.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using TelegramClient.MVVM.ViewModel;
 
 namespace TelegramClone
 {
@@ -13,5 +9,24 @@ namespace TelegramClone
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<MainWindow>(provider => new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainWindowViewModel>()
+            });
+            services.AddSingleton<MainWindowViewModel>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ TelegramServer/DB/DBConnection.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace TelegramServer.DB
+{
+    public class DBConnection
+    {
+        MySqlConnection connection;
+        public DBConnection()
+        {
+            connection = new MySqlConnection("sever=localhost;port=3306;user=root;" +
+                "password=root;database=tgserverdb");
+        }
+        public void OpenConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+    }
+}
